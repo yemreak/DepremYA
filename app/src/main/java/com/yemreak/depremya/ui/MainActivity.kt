828bcd9f1988c952428330019e835acba7df6efe
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2020-01-29 22:18:09 +0300

    👨‍💻 Kodlama güzelleştirmeleri

--- app/src/main/java/com/yemreak/depremya/ui/MainActivity.kt
+++ app/src/main/java/com/yemreak/depremya/ui/MainActivity.kt
@@ -9,21 +9,21 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
-		initRecyclerView()
-		quake_refresh_layout.setOnRefreshListener {
-			initRecyclerView()
-		}
-	}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initRecyclerView()
+        quake_refresh_layout.setOnRefreshListener {
+            initRecyclerView()
+        }
+    }
 
-	private fun initRecyclerView() {
-		quake_recycler_view.layoutManager = LinearLayoutManager(this)
-		KandilliAPI.requestEarthQuakes(this) {
-			quake_recycler_view.adapter = QuakeAdapter(this, it)
-			quake_refresh_layout.isRefreshing = false
-		}
-	}
+    private fun initRecyclerView() {
+        quake_recycler_view.layoutManager = LinearLayoutManager(this)
+        KandilliAPI.requestEarthQuakes(this) {
+            quake_recycler_view.adapter = QuakeAdapter(this, it)
+            quake_refresh_layout.isRefreshing = false
+        }
+    }
 
 }
