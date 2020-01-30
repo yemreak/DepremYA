8e2ff615bf1d89e0580634fcbc62306f11f40186
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-01-30 23:59:07 +0300

    🤹‍♀️ Filtreleme fonksiyonelliği eklendi (#21)

--- app/src/main/java/com/yemreak/depremya/ui/MainActivity.kt
+++ app/src/main/java/com/yemreak/depremya/ui/MainActivity.kt
@@ -1,18 +1,31 @@
 package com.yemreak.depremya.ui
 
+import android.app.Dialog
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.yemreak.depremya.R
 import com.yemreak.depremya.api.KandilliAPI
+import com.yemreak.depremya.entity.Earthquake
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.city_filter_dialog.*
+import kotlinx.android.synthetic.main.mag_filter_dialog.*
 
 class MainActivity : AppCompatActivity() {
-	
+	//private var sbMag: SeekBar? = null
+	private val MIN_MAG = 0
+	private val MAX_MAG = 10
+	private var quakes: List<Earthquake> = emptyList()
+	private var minMag: Int = 0
+	private var maxMag: Int = 0
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 		initRecyclerView()
+		initNavDrawer()
 		quake_refresh_layout.setOnRefreshListener {
 			initRecyclerView()
 		}
@@ -21,9 +34,88 @@ class MainActivity : AppCompatActivity() {
 	private fun initRecyclerView() {
 		quake_recycler_view.layoutManager = LinearLayoutManager(this)
 		KandilliAPI.requestEarthQuakes(this) {
+			quakes = it
 			quake_recycler_view.adapter = QuakeAdapter(this, it)
 			quake_refresh_layout.isRefreshing = false
 		}
 	}
 	
+	private fun initNavDrawer() {
+		val actionBarDrawerToggle = ActionBarDrawerToggle(
+			this, drawerLayout, R.string
+				.str_open, R.string.str_close
+		)
+		actionBarDrawerToggle.isDrawerIndicatorEnabled = true
+		
+		drawerLayout.addDrawerListener(actionBarDrawerToggle)
+		actionBarDrawerToggle.syncState()
+		supportActionBar?.setDisplayHomeAsUpEnabled(true)
+		navView?.setNavigationItemSelectedListener {
+			when (it.itemId) {
+				R.id.city_filter -> {
+					buildDialog(R.id.city_filter)
+					true
+				}
+				R.id.mag_filter -> {
+					buildDialog(R.id.mag_filter)
+					true
+				}
+				else -> true
+			}
+		}
+	}
+	
+	private fun buildDialog(option: Int) {
+		val dialog: Dialog = Dialog(this)
+		if (option == R.id.mag_filter) {
+			dialog.setContentView(R.layout.mag_filter_dialog)
+			initNumPickers(dialog)
+			dialog.btnAcceptMag.setOnClickListener {
+				quake_recycler_view.adapter = QuakeAdapter(this, quakes.filter {
+					(it.ml.toDouble() >= minMag) && (it.ml.toDouble() <= maxMag)
+				})
+				dialog.dismiss()
+			}
+		} else {
+			dialog.setContentView(R.layout.city_filter_dialog)
+			var arrayAdapter = ArrayAdapter<String>(
+				this, android.R.layout.simple_list_item_1,
+				this.resources.getStringArray(R.array.tr_cities)
+			)
+			dialog.actvCity?.setAdapter(arrayAdapter)
+			dialog.btnAcceptCity?.setOnClickListener {
+				quake_recycler_view.adapter = QuakeAdapter(this, quakes.filter {
+					(it.city.equals(dialog.actvCity.text.toString(), ignoreCase = true))
+				})
+				dialog.dismiss()
+			}
+		}
+		dialog.show()
+		drawerLayout.closeDrawer(navView)
+		
+	}
+	
+	private fun initNumPickers(dialog: Dialog) {
+		dialog.npMin.minValue = MIN_MAG
+		dialog.npMin.maxValue = MAX_MAG
+		dialog.npMax.minValue = MIN_MAG
+		dialog.npMax.maxValue = MAX_MAG
+		dialog.npMax.value = MAX_MAG
+		dialog.npMin.setOnValueChangedListener() { picker, oldVal, newVal ->
+			minMag = newVal
+		}
+		dialog.npMax.setOnValueChangedListener() { picker, oldVal, newVal ->
+			maxMag = newVal
+		}
+	}
+	
+	override fun onOptionsItemSelected(item: MenuItem): Boolean {
+		val actionBarDrawerToggle = ActionBarDrawerToggle(
+			this, drawerLayout, R.string
+				.str_open, R.string.str_close
+		)
+		if (actionBarDrawerToggle.onOptionsItemSelected(item))
+			return true
+		return super.onOptionsItemSelected(item)
+	}
 }
