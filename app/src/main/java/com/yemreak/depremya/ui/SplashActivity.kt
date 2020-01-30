676f353564aa6a177845c564f6805803dcfa7d47
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2020-01-30 15:57:19 +0300

    👨‍💻 Tüm proje içeriği yeniden formatlandı (#19)
    * 👨‍🔧 Girinti için tab yapısı kullanıldı
    * 🔨 `.editorconfig` yapılandırması eklendi
    * ➕ Dosya sonu satırı eklendi
    * 👨‍🔧 Earthquake ismine geçildi

--- app/src/main/java/com/yemreak/depremya/ui/SplashActivity.kt
+++ app/src/main/java/com/yemreak/depremya/ui/SplashActivity.kt
@@ -9,25 +9,25 @@ import com.yemreak.depremya.R
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
-    val SPLASH_TIME = 2000
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        supportActionBar?.hide()
-        runProgressBar()
-        val handler = Handler()
-        handler.postDelayed(splashHandler, SPLASH_TIME.toLong())
-
-    }
-
-    internal var splashHandler: Runnable = Runnable {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
-
-    fun runProgressBar() {
-        ObjectAnimator.ofInt(pbSplash, "progress", 100).setDuration(SPLASH_TIME.toLong())
-            .start()
-    }
+	val SPLASH_TIME = 2000
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_splash)
+		supportActionBar?.hide()
+		runProgressBar()
+		val handler = Handler()
+		handler.postDelayed(splashHandler, SPLASH_TIME.toLong())
+		
+	}
+	
+	internal var splashHandler: Runnable = Runnable {
+		val intent = Intent(this, MainActivity::class.java)
+		startActivity(intent)
+		finish()
+	}
+	
+	fun runProgressBar() {
+		ObjectAnimator.ofInt(pbSplash, "progress", 100).setDuration(SPLASH_TIME.toLong())
+			.start()
+	}
 }
