3d2d9272ba2be37c79408f29180e77efa2283b29
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-02-01 16:55:29 +0300

    📡 İnternete bağlanmadı ekranı tamamlandı

--- app/src/main/java/com/yemreak/depremya/ui/MainActivity.kt
+++ app/src/main/java/com/yemreak/depremya/ui/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.yemreak.depremya.ui
 
 import android.app.Dialog
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.yemreak.depremya.R
@@ -12,17 +15,41 @@ import com.yemreak.depremya.api.KandilliAPI
 import com.yemreak.depremya.entity.Quake
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.filter_dialog.view.*
+import kotlinx.android.synthetic.main.urgent_layout.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 	private var quakes: List<Quake> = emptyList()
 	private var selectedMag: Int = 0
+	private var mainLayout: View? = null
+	private var urgentLayout: View? = null
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
-		initRecyclerView()
-		quake_refresh_layout.setOnRefreshListener {
+		mainLayout = layoutInflater.inflate(R.layout.activity_main, null)
+		urgentLayout = layoutInflater.inflate(R.layout.urgent_layout, null)
+		chooseLayout()
+	}
+	
+	private fun chooseLayout() {
+		if (checkConnection()) {
+			setContentView(mainLayout)
 			initRecyclerView()
+			quake_refresh_layout.setOnRefreshListener {
+				chooseLayout()
+			}
+		} else {
+			setContentView(urgentLayout)
+			initUrgentLayout(R.drawable.no_internet, R.string.no_internet)
+			urgent_refresh_layout.isRefreshing = false
+			urgent_refresh_layout.setOnRefreshListener {
+				chooseLayout()
+			}
 		}
+	}
+	
+	private fun initUrgentLayout(id: Int, text: Int) {
+		urgent_image?.setImageResource(id)
+		urgent_text?.setText(text)
 	}
 	
 	private fun initRecyclerView() {
@@ -68,5 +95,15 @@ class MainActivity : AppCompatActivity() {
 		if (item.itemId == R.id.menu_item_filter)
 			buildDialog()
 		return super.onOptionsItemSelected(item)
+	}
+	
+	fun checkConnection(): Boolean {
+		var connManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+		var networkInfo =
+			Objects.requireNonNull(connManager).getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+		var isWifiConn = Objects.requireNonNull(networkInfo).isConnected
+		networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)
+		var isMobileConn = Objects.requireNonNull(networkInfo).isConnected
+		return isWifiConn || isMobileConn
 	}
 }
