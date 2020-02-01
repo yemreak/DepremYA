bcbaafa1126590581db0ef9c94227d1c15815544
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2020-02-02 02:06:20 +0300

    🚄 DB verileri sıralandı

--- app/src/main/java/com/yemreak/depremya/db/dao/QuakeDao.kt
+++ app/src/main/java/com/yemreak/depremya/db/dao/QuakeDao.kt
@@ -13,10 +13,10 @@ abstract class QuakeDao {
 	@Insert
 	abstract suspend fun insertAll(quakes: Array<out Quake>)
 	
-	@Query("SELECT * FROM ${Quake.TABLE_NAME}")
+	@Query("SELECT * FROM ${Quake.TABLE_NAME} ORDER BY ${Quake.COLUMN_ID} ASC")
 	abstract fun getAll(): Flow<List<Quake>>
 	
-	@Query("SELECT * FROM ${Quake.TABLE_NAME} WHERE ${Quake.COLUMN_MD} > :md")
+	@Query("SELECT * FROM ${Quake.TABLE_NAME} WHERE ${Quake.COLUMN_MD} > :md ORDER BY ${Quake.COLUMN_ID} ASC")
 	abstract fun getAllHigherMd(md: Float): Flow<List<Quake>>
 	
 	@Query("DELETE FROM ${Quake.TABLE_NAME}")
