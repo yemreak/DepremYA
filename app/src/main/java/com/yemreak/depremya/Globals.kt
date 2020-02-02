1adc65737bd3051f4590f8e318f611236a63705c
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-02-02 18:47:48 +0300

    👷‍♀️ Globals sınıfı kuruldu

---
+++ app/src/main/java/com/yemreak/depremya/Globals.kt
@@ -0,0 +1,32 @@
+package com.yemreak.depremya
+
+import com.yemreak.depremya.db.entity.Quake
+
+public class Globals {
+	companion object {
+		var INSTANCE: Globals? = null
+	}
+	
+	private var quakes: List<Quake> = emptyList()
+	
+	object Singleton {
+		fun getInstance(): Globals {
+			if (INSTANCE == null) {
+				INSTANCE = Globals()
+			}
+			return INSTANCE as Globals
+		}
+	}
+	
+	@Throws(NullPointerException::class)
+	public fun getQuakes(): List<Quake> {
+		if (quakes.isEmpty()) {
+			throw NullPointerException("The selected news invoked without creation")
+		}
+		return quakes
+	}
+	
+	public fun setQuakes(quakeList: List<Quake>) {
+		quakes = quakeList
+	}
+}
