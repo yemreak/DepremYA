904da0d5c3d2eaeb4bc34344c470109e945d3392
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-02-02 12:44:01 +0300

    👷‍♀️ Detaylı arayüz kuruldu

--- app/src/main/java/com/yemreak/depremya/ui/QuakeAdapter.kt
+++ app/src/main/java/com/yemreak/depremya/ui/QuakeAdapter.kt
@@ -1,5 +1,6 @@
 package com.yemreak.depremya.ui
 
+import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import android.graphics.PorterDuff
@@ -12,11 +13,16 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.yemreak.depremya.R
 import com.yemreak.depremya.db.entity.Quake
+import kotlinx.android.synthetic.main.detailed_quake.view.*
 import kotlinx.android.synthetic.main.quake_item.view.*
+import kotlinx.android.synthetic.main.quake_item.view.tvCity
+import kotlinx.android.synthetic.main.quake_item.view.tvDate
+import kotlinx.android.synthetic.main.quake_item.view.tvHour
+import kotlinx.android.synthetic.main.quake_item.view.tvMl
+import kotlinx.android.synthetic.main.quake_item.view.tvRegion
 
-class QuakeAdapter(private val context: Context, private var quakes: List<Quake>) :
+class QuakeAdapter(val context: Context, private var quakes: List<Quake>) :
 	RecyclerView.Adapter<QuakeAdapter.QuakeHolder>() {
-	
 	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuakeAdapter.QuakeHolder {
 		val view: View =
 			LayoutInflater.from(parent.context).inflate(R.layout.quake_item, parent, false)
@@ -34,18 +40,8 @@ class QuakeAdapter(private val context: Context, private var quakes: List<Quake>
 			else
 				holder.tvRegion.text =
 					context.getString(R.string.str_region, region)
-			
-			holder.ibLocation.setOnClickListener {
-				val uri = "https://maps.google.com/?q=${lat},${lng}&ll=${lat},${lng}&z=7"
-				val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
-				intent.setPackage("com.google.android.apps.maps")
-				if (intent.resolveActivity(context.packageManager) != null)
-					context.startActivity(intent)
-			}
-			holder.tvMl.background.setColorFilter(
-				context.resources.getColor(generateMagColor(ml.toDouble())),
-				PorterDuff.Mode.SRC_IN
-			)
+			openMaps(holder.ibLocation, lat, lng)
+			setMagColor(holder.tvMl, ml.toDouble())
 		}
 	}
 	
@@ -54,8 +50,67 @@ class QuakeAdapter(private val context: Context, private var quakes: List<Quake>
 		notifyDataSetChanged()
 	}
 	
-	private fun generateMagColor(mag: Double): Int {
-		return when {
+	
+	override fun getItemCount() = quakes.size
+	
+	inner class QuakeHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+		val tvDate: TextView = itemView.tvDate
+		val tvHour: TextView = itemView.tvHour
+		val tvMl: TextView = itemView.tvMl
+		val tvCity: TextView = itemView.tvCity
+		val tvRegion: TextView = itemView.tvRegion
+		val ibLocation: ImageButton = itemView.ibLocation
+		val view: View? = null
+		
+		init {
+			itemView.setOnClickListener {
+				val pos = adapterPosition
+				//val sendInent = Intent().setAction(Intent.ACTION_SEND)
+				val filterDialog = Dialog(it.context)
+				val view =
+					LayoutInflater
+						.from(filterDialog.context)
+						.inflate(R.layout.detailed_quake, null)
+				filterDialog
+					.setContentView(view)
+				initDialog(view, pos)
+				filterDialog.show()
+			}
+		}
+		
+		private fun initDialog(view: View, pos: Int) {
+			with(quakes[pos]) {
+				view.tvMl.text = ml
+				view.tvHour.text = context.getString(R.string.str_hour, hour)
+				view.tvDate.text = date
+				view.tvDepth.text = context.getString(R.string.str_depth, depth)
+				view.tvResolution.text = context.getString(R.string.str_resolution, resolution)
+				view.tvMd.text = context.getString(R.string.str_md, md)
+				view.tvMw.text = context.getString(R.string.str_mw, mw)
+				view.tvCity.text = city
+				if (region == "")
+					view.tvRegion.text = context.getString(R.string.str_region, " - ")
+				else
+					view.tvRegion.text =
+						context.getString(R.string.str_region, region)
+				openMaps(view.ibLocationDetailed, lat, lng)
+				setMagColor(view.tvMl, ml.toDouble())
+			}
+		}
+	}
+	
+	fun openMaps(imageButton: ImageButton, lat: String, lng: String) {
+		imageButton.setOnClickListener {
+			val uri = "https://maps.google.com/?q=${lat},${lng}&ll=${lat},${lng}&z=7"
+			val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+			intent.setPackage("com.google.android.apps.maps")
+			if (intent.resolveActivity(context.packageManager) != null)
+				context.startActivity(intent)
+		}
+	}
+	
+	private fun setMagColor(textView: TextView, mag: Double) {
+		val color = when {
 			mag < 4 -> R.color.quake0_4
 			mag >= 4 && mag < 5 -> R.color.quake4_5
 			mag >= 5 && mag < 5.5 -> R.color.quake5_5h
@@ -67,17 +122,9 @@ class QuakeAdapter(private val context: Context, private var quakes: List<Quake>
 				R.color.quakeMax
 			}
 		}
+		textView.background.setColorFilter(
+			context.resources.getColor(color),
+			PorterDuff.Mode.SRC_IN
+		)
 	}
-	
-	override fun getItemCount() = quakes.size
-	
-	class QuakeHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-		val tvDate: TextView = itemView.tvDate
-		val tvHour: TextView = itemView.tvHour
-		val tvMl: TextView = itemView.tvMl
-		val tvCity: TextView = itemView.tvCity
-		val tvRegion: TextView = itemView.tvRegion
-		val ibLocation: ImageButton = itemView.ibLocation
-	}
-	
 }
