d53b9c05c1dbdc36b97d90115dc92606a1935a70
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-02-02 18:15:22 +0300

    👷‍♀️ Yeni arayüzler eklendi (#33)

--- app/src/main/java/com/yemreak/depremya/ui/MainActivity.kt
+++ app/src/main/java/com/yemreak/depremya/ui/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.yemreak.depremya.ui
 
 import android.app.Dialog
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -16,47 +19,63 @@ import com.yemreak.depremya.db.entity.Quake
 import com.yemreak.depremya.viewmodel.QuakeViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.filter_dialog.view.*
+import kotlinx.android.synthetic.main.urgent_layout.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 	
 	private var quakes: List<Quake> = emptyList()
 	private var selectedMag: Int = 0
-	
+	private var mainLayout: View? = null
+	private var urgentLayout: View? = null
+
 	private lateinit var quakeViewModel: QuakeViewModel
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
-		
-		initRecyclerView()
-		
+
+		mainLayout = layoutInflater.inflate(R.layout.activity_main, null)
+		urgentLayout = layoutInflater.inflate(R.layout.urgent_layout, null)
+		getData()
+	}
+	
+	private fun getData() {
 		quakeViewModel = ViewModelProvider(this).get(QuakeViewModel::class.java)
 		quakeViewModel.allQuakes.observe(this, Observer {
 			it?.let {
-				quakes = it
-				(quake_recycler_view.adapter as QuakeAdapter).setQuakesAndNotify(quakes)
+				if (it.isEmpty() && !checkConnection()) {
+					// ilk çalıştırma ve internet yok
+					setContentView(urgentLayout)
+					initUrgentLayout(R.drawable.no_internet, R.string.no_internet)
+				} else {
+					quakes = it
+					setContentView(mainLayout)
+					initRecyclerView()
+				}
 			}
 		})
-		
 		if (quakes.isEmpty()) refreshData()
-		
-		quake_refresh_layout.setOnRefreshListener {
-			refreshData()
-			quake_refresh_layout.isRefreshing = false
-		}
 	}
 	
 	private fun refreshData() {
 		KandilliAPI.requestEarthQuakes(this) {
 			when {
-				it == null -> // TODO: 2/2/2020 Asmaa Mirkhan - Bu alana internet olmadığındaki UI gelecek
+				it == null -> {
+					// layout'un değişmesi "Bad behaviour" olabilir, kullanıcı elindeki verileri kaybetmiş gibi hissedecek
+					// Toast daha verimli olabilir
+					// setContentView(urgentLayout)
+					// initUrgentLayout(R.drawable.no_internet, R.string.no_internet)
 					Toast.makeText(
 						this,
-						"Bağlantı başarısız",
+						R.string.conn_fail,
 						Toast.LENGTH_SHORT
 					).show()
-				quakes.isEmpty() || it.first() != quakes.first() ->
+				}
+				quakes.isEmpty() || it.first() != quakes.first() -> {
+					setContentView(mainLayout)
+					initRecyclerView()
 					quakeViewModel.refreshQuakes(it)
+				}
 			}
 		}
 	}
@@ -64,6 +83,19 @@ class MainActivity : AppCompatActivity() {
 	private fun initRecyclerView() {
 		quake_recycler_view.layoutManager = LinearLayoutManager(this)
 		quake_recycler_view.adapter = QuakeAdapter(this, quakes)
+		quake_refresh_layout.setOnRefreshListener {
+			refreshData()
+			quake_refresh_layout.isRefreshing = false
+		}
+	}
+	
+	private fun initUrgentLayout(id: Int, text: Int) {
+		urgent_image?.setImageResource(id)
+		urgent_text?.setText(text)
+		urgent_refresh_layout.setOnRefreshListener {
+			refreshData()
+			urgent_refresh_layout.isRefreshing = false
+		}
 	}
 	
 	private fun buildDialog() {
@@ -84,8 +116,22 @@ class MainActivity : AppCompatActivity() {
 				R.id.btnGreater6 -> 6
 				else -> 0
 			}
-			(quake_recycler_view.adapter as QuakeAdapter)
-				.setQuakesAndNotify(quakes.filter { it.ml.toDouble() >= selectedMag })
+			val filtered = quakes.filter {
+				(it.ml.toDouble() >= selectedMag)
+			}
+			if (filtered.isEmpty()) {
+				setContentView(urgentLayout)
+				initUrgentLayout(R.drawable.magnifier, R.string.no_result)
+			} else {
+				if (quakes.isNotEmpty()) {
+					setContentView(mainLayout)
+					(quake_recycler_view?.adapter as QuakeAdapter)
+						.setQuakesAndNotify(filtered)
+					// TODO: 2/2/2020 Asmaa Mirkhan - Bunu kaldırıp kendi arayüzünden alınan limit ile bu metodu çalıştır
+					quakeViewModel.syncData(1, quakes.first())
+				}
+			}
+
 			filterDialog.dismiss()
 			
 			// TODO: 2/2/2020 Asmaa Mirkhan - Bunu kaldırıp kendi arayüzünden alınan limit ile bu metodu çalıştır
@@ -102,5 +148,15 @@ class MainActivity : AppCompatActivity() {
 		if (item.itemId == R.id.menu_item_filter)
 			buildDialog()
 		return super.onOptionsItemSelected(item)
+	}
+	
+	private fun checkConnection(): Boolean {
+		var connManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+		var networkInfo =
+			Objects.requireNonNull(connManager).getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+		var isWifiConn = Objects.requireNonNull(networkInfo).isConnected
+		networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)
+		var isMobileConn = Objects.requireNonNull(networkInfo).isConnected
+		return isWifiConn || isMobileConn
 	}
 }
